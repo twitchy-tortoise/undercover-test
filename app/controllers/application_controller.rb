27c76fcfb95c088d5e56bf055905901c36d87935
Jan Grodowski <jgrodowski@gmail.com>
2020-04-13 11:44:17 +0200

    tmp remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  private
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-  helper_method :current_user
-
-  def check_current_user
-    redirect_to root_url if current_user.nil?
-  end
 end
