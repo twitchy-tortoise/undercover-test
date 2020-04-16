220d7b0557705111fb048dd3fa08566fd42436e3
Jan Grodowski <jgrodowski@gmail.com>
2020-04-16 11:09:22 +0200

    trigger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   private
 
+  def auth_and_parse
+    check_current_user
+    parse_and_validate_webhook(request.body.read)
+  end
+
   def check_current_user
     redirect_to root_url if current_user.nil?
   end
