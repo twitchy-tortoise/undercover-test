b05e7233658c117844ab088fd138643a77cb9750
Jan Grodowski <jgrodowski@gmail.com>
2020-04-16 11:33:35 +0200

    foo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
     parse_and_validate_webhook(request.body.read)
   end
 
+  def auth_and_parse
+    check_current_user
+    parse_and_validate_webhook(request.body.read)
+  end
+
   def check_current_user
     redirect_to root_url if current_user.nil?
   end
